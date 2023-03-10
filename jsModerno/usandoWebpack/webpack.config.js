c73daea9477a2d7246c8c4a9c675d94939fae0b9
andreymafra55 <andreymafra55@gmail.com>
2023-03-09 23:11:28 -0300

    aprendendo a configurar o webpack

---
+++ jsModerno/usandoWebpack/webpack.config.js
@@ -0,0 +1,14 @@
+const { dirname } = require('path')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    hello: './src/hello.js'
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname,'public'),
+    filename: '[name].min.js'
+  }
+}
