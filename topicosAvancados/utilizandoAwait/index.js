8bf29d43a64b8f32abff61d7a7961a4988619379
andreymafra55 <andreymafra55@gmail.com>
2023-03-15 21:26:28 -0300

    utilizando o await

---
+++ topicosAvancados/utilizandoAwait/index.js
@@ -0,0 +1,16 @@
+async function asyncSum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return Promise.reject("arguments must be of type number");
+  }
+  return a + b;
+}
+
+async function execute() {
+  try {
+    const result = await asyncSum(50, null);
+    console.log(result);
+  } catch (error) {
+    console.log(error)   
+  }
+}
+execute();
